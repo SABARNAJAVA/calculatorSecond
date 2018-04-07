f14f78f2f151ea616ac2dabd4e75f639aded5c7d
Hp <Hp@pc-PC>
2018-04-08 01:44:44 +0530

    first commit

---
+++ secondTry/src/secondTry/calculator.java
@@ -0,0 +1,18 @@
+package secondTry;
+
+
+
+public class calculator 
+{
+public int add(int parameter1,int parameter2)
+{
+	return parameter1 + parameter2;
+}
+public double divide(int divisor,int dividend)
+{
+	
+		double quotient = dividend/divisor;
+		return quotient;	
+	
+}
+}
